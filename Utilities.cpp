a6683b27f70df012dc8b4c2754c43f4c054e2696
Amila Rathnayake <AR7142@zebra.com>
2018-10-26 08:41:39 +0530

    content added

--- Utilities.cpp
+++ Utilities.cpp
@@ -110,3 +110,27 @@ std::wstring ReadFileAsUnicode(const std::wstring& filePath)
 
 	return ws;
 }
+
+static inline std::string tolower(std::string str)
+{
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+	return str;
+}
+
+static inline std::string &ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
+
+static inline std::string &rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+static inline std::string &trim(std::string &s) {
+	return ltrim(rtrim(s));
+}
+
+static inline std::string &RemoveWhiteSpaces(std::string &s) {
+	return ltrim(rtrim(s));
+}
