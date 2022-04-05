321d55d7995afc30db782b268e3b350d3959eb2e
Amila Rathnayake <amilamad@gmail.com>
2022-04-05 19:48:56 -0400

    Update Utilities.cpp

--- Utilities.cpp
+++ Utilities.cpp
@@ -15,6 +15,22 @@ enum TextEncoding
 	UTF_32
 };
 
+std::vector<std::string> splitString(std::string str, const std::string token){
+    std::vector<std::string>result;
+    while(str.size()){
+        int index = str.find(token);
+        if(index != std::string::npos){
+            result.push_back(str.substr(0,index));
+            str = str.substr(index+token.size());
+            if(str.size()==0)result.push_back(str);
+        }else{
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
+}
+
 std::wstring convertStringToWstring(const std::string& str) 
 { 
 	using namespace std;
